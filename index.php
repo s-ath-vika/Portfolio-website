794551c4e2c5d4521fecdc961cf5b5adc2f1a4a6
sathvika <sathvika1625@gmail.com>
2026-06-04 21:47:04 +0530

    Refactored header layout using clean modular PHP include statements

--- index.php
+++ index.php
@@ -10,14 +10,7 @@
 
     <header>
         <div class="logo"><strong>Portfolio</strong></div>
-        <nav>
-            <ul style="display: flex; list-style: none; gap: 15px;">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
+        <?php include('navbar.php'); ?>
     </header>
 
     <main>
---
+++ navbar.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul style="display: flex; list-style: none; gap: 15px;">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+</nav>
